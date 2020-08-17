55500cc29bf7401df3e8aa9cc1519bff1a378f05
Nikita Teremovskii <ni-2@mail.ru>
2020-08-17 22:43:50 +0300

    Fixed linter errors

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -9,12 +9,11 @@ console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const rightAnswersNumber = 3;
 for (let i = 0; i < rightAnswersNumber; i += 1) {
-	const [result, message] = gameIsEven();
-	console.log(message);
-	if (!result) {
-		console.log(`Let's try again, ${name}!`);
-		break;
-	}
-	if (i === 2) console.log(`Congratulations, ${name}!`);
-};
-
+  const [result, message] = gameIsEven();
+  console.log(message);
+  if (!result) {
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+  if (i === 2) console.log(`Congratulations, ${name}!`);
+}
