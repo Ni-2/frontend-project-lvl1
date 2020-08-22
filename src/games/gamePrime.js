42c8c90812de4f862fb730f5bf3b2386ad55bc48
Nikita Teremovskii <ni-2@mail.ru>
2020-08-22 21:57:50 +0300

    Changed logic of gamePrime.js

--- src/games/gamePrime.js
+++ src/games/gamePrime.js
@@ -2,19 +2,11 @@ import _ from 'lodash';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-// Based on Sieve of Eratosthenes algorithm.
-const primeNumbers = (limit) => {
-  const numbers = _.range(2, limit + 1);
-  const numBoolDict = numbers.reduce((acc, num) => ({ ...acc, [num]: true }), {});
-  for (let i = 2; i ** 2 <= limit; i += 1) {
-    if (numBoolDict[i]) {
-      for (let j = i ** 2; j <= limit; j += i) numBoolDict[j] = false;
-    }
-  }
-  return Object.entries(numBoolDict).filter(([, bool]) => bool).map(([num]) => +num);
+const isPrime = (num) => {
+  if (num === 1) return false;
+  for (let i = 2; i ** 2 <= num; i += 1) if (num % i === 0) return false;
+  return true;
 };
-
-const isPrime = (num) => primeNumbers(num).includes(num);
 
 export default (n) => {
   const randomNumber = _.random(1, n);
