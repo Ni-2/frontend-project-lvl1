74f1dd88a73ae2512037da850b1e46f4134ecc32
Nikita Teremovskii <ni-2@mail.ru>
2020-08-23 19:28:49 +0300

    Fixed error in gameProgression.js

--- src/games/gameProgression.js
+++ src/games/gameProgression.js
@@ -10,8 +10,8 @@ const game = (n) => {
   const upperBorder = random(firstNumber + (progressionLength - 1), n);
   const step = Math.floor((upperBorder - firstNumber) / (progressionLength - 1));
   const arrOfNumbers = range(firstNumber, upperBorder, step).slice(0, progressionLength);
-  const index = random(0, progressionLength - 1);
-  const answer = arrOfNumbers.splice(index, 1, '..').join('');
+  const answerIndex = random(0, progressionLength - 1);
+  const answer = arrOfNumbers.splice(answerIndex, 1, '..').join('');
   return [task, arrOfNumbers.join(' '), answer];
 };
 
