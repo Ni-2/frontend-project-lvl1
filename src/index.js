d2fb2beac573371827b5c8913e126921b4bf3eab
Nikita Teremovskii <ni-2@mail.ru>
2020-08-28 00:44:35 +0300

    Refractored index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ export default (getTask, genRoundData) => {
   console.log('Welcone to the Brain Games!');
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log(getTask());
   for (let i = 0; i < attemptCount; i += 1) {
     const [question, rightAnswer] = genRoundData();
-    if (i === 0) console.log(getTask());
     const usersAnswer = readLineSync.question(`Qwestion: ${question}\nYour answer: `);
     if (usersAnswer === rightAnswer) console.log('Correct!');
     else {
       console.log(`"${usersAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
       console.log(`Let's try again, ${name}!`);
-      break;
+      return;
     }
-    if (i === attemptCount - 1) console.log(`Congratulations, ${name}!`);
   }
+  console.log(`Congratulations, ${name}!`);
 };
