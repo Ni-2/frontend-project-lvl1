bec6f99c39aa351706905c86b513fccb9bff5f3a
Nikita Teremovskii <ni-2@mail.ru>
2020-08-27 18:29:47 +0300

    Refactored gamePrime.js

--- src/games/gamePrime.js
+++ src/games/gamePrime.js
@@ -5,7 +5,7 @@ const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getTask = () => task;
 
 const isPrime = (num) => {
-  if (num === 1) return false;
+  if (num <== 1) return false;
   for (let i = 2; i ** 2 <= num; i += 1) if (num % i === 0) return false;
   return true;
 };
