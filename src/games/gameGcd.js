6db5bd0f7fe7dd59141810869c31f91dcd1b86c3
Nikita Teremovskii <ni-2@mail.ru>
2020-08-27 23:28:20 +0300

    Refactored gameGcd.js

--- src/games/gameGcd.js
+++ src/games/gameGcd.js
@@ -6,21 +6,16 @@ const getTask = () => task;
 
 // Set the upper border of the range of numbers in the game.
 const numbersUpperBorder = 99;
-
+const gcd = (firstNum, secondNum) => (
+  secondNum === 0 ? firstNum
+    : gcd(secondNum, firstNum % secondNum)
+);
 const genRoundData = () => {
   const firstNumber = random(1, numbersUpperBorder);
   const secondNumber = random(1, numbersUpperBorder);
-  const message = `${firstNumber}, ${secondNumber}`;
-  let [maxNumber, minNumber] = firstNumber >= secondNumber
-    ? [firstNumber, secondNumber]
-    : [secondNumber, firstNumber];
-  for (;;) {
-    const tail = maxNumber % minNumber;
-    if (tail === 0) return [message, minNumber.toString()];
-    if (tail === 1) return [message, '1'];
-    maxNumber = minNumber;
-    minNumber = tail;
-  }
+  const question = `${firstNumber}, ${secondNumber}`;
+  const rightAnswer = gcd(firstNumber, secondNumber);
+  return [question, rightAnswer];
 };
 
 export default () => startGameEngine(getTask, genRoundData);
